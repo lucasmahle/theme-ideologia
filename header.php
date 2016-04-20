21c46a6ccc6757dd2361aea154152ae7adf34d86
Lucas Mahle <lucassmahle@gmail.com>
2016-04-20 15:56:37 -0300

    First Commit

---
+++ header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+		<title>Título</title>
+		<!-- STYLES -->
+		<link rel="stylesheet" id="google-fonts-css" href="//fonts.googleapis.com/css?family=Roboto+Slab:400,300,700" type="text/css" media="all">
+		<link rel="stylesheet/less" href="assets/css/style.less">
+	
+		<!-- SCRIPTS -->
+			<script>less={logLevel:0}</script>
+		<!-- VENDOR -->
+			<script src="assets/js/less.js"></script>
+			
+		<script src="bower_components/jquery/dist/jquery.min.js"></script>
+		<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+		<script src="assets/js/main.js"></script>
+	</head>
+	<body>
+		<header class="navbar blog-navbar navbar-default navbar-fixed-top" id="top">
+			<div class="container">
+				<div class="navbar-header">
+					<button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#blog-navbar" aria-controls="blog-navbar" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div>
+				<nav class="collapse navbar-collapse">
+					<a href="#">
+						<img src="assets/img/logo-blog.png" class="logo">
+					</a>
+					<div class="navbar-itens">
+						<ul class="nav navbar-nav">
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Categorias <span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="#">Angular JS</a></li>
+									<li><a href="#">CSS</a></li>
+									<li><a href="#">Laravel</a></li>
+									<li><a href="#">PHP</a></li>
+								</ul>
+							</li>
+							<li>
+								<a href="#">Sobre mim</a>
+							</li>
+							<li>
+								<a href="#">Contato</a>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		</header>
---
+++ post.php
@@ -0,0 +1,15 @@
+<?php require_once('header.php'); ?>
+    
+    <div class="container">
+        <article class="page-content">
+            <header class="post-header">
+                <img src="posts-image/post (5).png" class="img-responsive">
+            </header>
+        </article>
+        
+        <section class="comments">
+            comentario
+        </section>
+    </div>
+    
+<?php require_once('footer.php'); ?>
